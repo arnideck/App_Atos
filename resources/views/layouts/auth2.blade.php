cf14749012a33366a35fb766d1f564873d0280ab
unknown <arnideck@gmail.com>
2024-03-19 23:10:58 -0300

    logo

--- resources/views/layouts/auth2.blade.php
+++ resources/views/layouts/auth2.blade.php
@@ -64,23 +64,7 @@
                     </select>
                 </div>
                 
-               --> 
-                
-                <div class="col-md-9 col-xs-8" style="text-align: right;padding-top: 10px;">
-                    @if(!($request->segment(1) == 'business' && $request->segment(2) == 'register'))
-                        <!-- Register Url -->
-                        @if(config('constants.allow_registration'))
-                            <a href="{{ route('business.getRegister') }}@if(!empty(request()->lang)){{'?lang=' . request()->lang}} @endif" class="btn bg-maroon btn-flat" ><b>{{ __('business.not_yet_registered')}}</b> {{ __('business.register_now') }}</a>
-                            <!-- pricing url -->
-                            @if(Route::has('pricing') && config('app.env') != 'demo' && $request->segment(1) != 'pricing')
-                                &nbsp; <a href="{{ action('\Modules\Superadmin\Http\Controllers\PricingController@index') }}">@lang('superadmin::lang.pricing')</a>
-                            @endif
-                        @endif
-                    @endif
-                    @if($request->segment(1) != 'login')
-                        &nbsp; &nbsp;<span class="text-white">{{ __('business.already_registered')}} </span><a href="{{ action('Auth\LoginController@login') }}@if(!empty(request()->lang)){{'?lang=' . request()->lang}} @endif">{{ __('business.sign_in') }}</a>
-                    @endif
-                </div>
+               -->
                 
                 @yield('content')
                 </div>
--- resources/views/layouts/partials/header-auth.blade.php
+++ resources/views/layouts/partials/header-auth.blade.php
@@ -25,17 +25,7 @@
 	        	@if(!($request->segment(1) == 'business' && $request->segment(2) == 'register'))
 
 	        		<!-- Register Url -->
-		        	@if(config('constants.allow_registration'))
-		            	<a 
-		            		href="{{ route('business.getRegister') }}@if(!empty(request()->lang)){{'?lang=' . request()->lang}} @endif"
-		            		class="btn bg-maroon btn-flat margin" 
-		            	><b>{{ __('business.not_yet_registered')}}</b> {{ __('business.register_now') }}</a>
-
-		            	<!-- pricing url -->
-			            @if(Route::has('pricing') && config('app.env') != 'demo' && $request->segment(1) != 'pricing')
-		                	<a href="{{ action('\Modules\Superadmin\Http\Controllers\PricingController@index') }}">@lang('superadmin::lang.pricing')</a>
-		            	@endif
-		            @endif
+		        	
 		        @endif
 
 		        @if(!($request->segment(1) == 'business' && $request->segment(2) == 'register') && $request->segment(1) != 'login')
